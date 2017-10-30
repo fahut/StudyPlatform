becaec109c7408b71a8c01fdb75e95d5f9e9c951
fahut <jnielsona@gmail.com>
2017-10-30 19:48:23 +0100

    qaSWAGHJKGYUK

---
+++ app/src/main/java/com/example/jonas/studyplatform/DatabaseHelper.java
@@ -0,0 +1,8 @@
+package com.example.jonas.studyplatform;
+
+/**
+ * Created by Jonas on 30/10/2017.
+ */
+
+public class DatabaseHelper {
+}
--- app/src/main/java/com/example/jonas/studyplatform/User.java
+++ app/src/main/java/com/example/jonas/studyplatform/User.java
@@ -5,4 +5,50 @@ package com.example.jonas.studyplatform;
  */
 
 public class User {
+    int id;
+    String username;
+    String email;
+    String name;
+    String password;
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+    public int getId()
+    {
+        return this.id;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+    public String getUsername()
+    {
+        return this.username;
+    }
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+    public String getEmail()
+    {
+        return this.email;
+    }
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+    public String getPassword()
+    {
+        return this.password;
+    }
 }
