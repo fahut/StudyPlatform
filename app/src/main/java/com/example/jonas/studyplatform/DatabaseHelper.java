b58aa68829c2806fab6e9374ae3dd826d21525d1
fahut <jnielsona@gmail.com>
2017-10-30 20:06:11 +0100

    qaSWAGHJKGYUK

--- app/src/main/java/com/example/jonas/studyplatform/DatabaseHelper.java
+++ app/src/main/java/com/example/jonas/studyplatform/DatabaseHelper.java
@@ -1,8 +1,42 @@
 package com.example.jonas.studyplatform;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Jonas on 30/10/2017.
  */
 
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "socialed.db";
+    private static final String TABLE_NAME = "users";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_USERNAME = "username";
+    private static final String COLUMN_PASSWORD = "password";
+    SQLiteDatabase db;
+    private static final String TABLE_CREATE = "create table users (id integer primary key not null auto_increment , " +
+            "name text not null , email text not null , username text not null , password text not null);";
+
+    public DatabaseHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+        this.db = db;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query = "DROP TABLE IF EXISTS "+TABLE_NAME;
+        db.execSQL(query);
+        this.onCreate(db);
+    }
 }
